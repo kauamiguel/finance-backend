5302eda43e77fca76d29b1266d6c2d214f5779b5
kauamiguel <kauamcm@gmail.com>
2026-03-29 15:44:46 -0300

    feat: add expenses route

--- app/main.py
+++ app/main.py
@@ -4,10 +4,11 @@ from sqlalchemy.orm.session import Session
 from app.schemas.schemas import UserPublicloginScheme, UserloginScheme, CategorySchema, ExpenseSchema
 from app.model.datamodel import Category, Expense
 from app.database_config import Base, engine, get_db
-from app.routers import categories
+from app.routers import categories, expenses
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
 app.include_router(categories.router)
+app.include_router(expenses.router)
